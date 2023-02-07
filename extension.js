644b3d5780e16ab2045576a076c7580f24095d03
Freddie <freddiejscott@outlook.com>
2023-02-07 19:09:23 +0000

    Add Break Mode

--- extension.js
+++ extension.js
@@ -1,20 +1,39 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
 
-let extensionEnabled = true;
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-function toggleVSPlus() {
-	extensionEnabled = !extensionEnabled
-}
+const vscode = require('vscode');
+const fs = require('fs')
 /**
  * @param {vscode.ExtensionContext} context
  */
-function activate(context) {
+function BreakMode() {
+	const panel = vscode.window.createWebviewPanel(
+		'break',
+		'Have a break',
+		vscode.ViewColumn.Active,
+		{
+			enableScripts: true,
+		}
+	);
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
+	let html = `<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Break Mode!</title>
+	</head>
+	<body>
+		<h1>Test!</h1>
+	</body>
+	</html>`
+	panel.webview.html = html
+}
+function activate(context) {
+	context.subscriptions.push(
+		vscode.commands.registerCommand("visual-studio-plus.breakMode", () => {
+			BreakMode()
+		})
+	)
 	console.log('Visual Studio + is now running!');
 }
 
